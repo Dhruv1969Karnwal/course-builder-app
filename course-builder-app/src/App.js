a7a0c6e9081b9b7fe759bfc7f9f18dc2c68d9b3f
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-07-02 18:35:28 +0530

    Course-page-added@18:35

--- course-builder-app/src/App.js
+++ course-builder-app/src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Route path='/courses' element={<Courses />} />
       </Routes>
     </Router>
+
   );
 }
 
--- course-builder-app/src/components/courses/Courses.jsx
+++ course-builder-app/src/components/courses/Courses.jsx
@@ -1,9 +1,137 @@
-import React from 'react'
+import {
+  Button,
+  Container,
+  HStack,
+  Heading,
+  Image,
+  Input,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
+import "./Course.css"
+
+const Course = ({
+  views,
+  title,
+  imageSrc,
+  id,
+  addToPlaylistHandler,
+  creator,
+  description,
+  lectureCount,
+}) => {
+  return (
+    <VStack className="course" alignItems={['center', 'flex-start']}>
+      <Image src={imageSrc} boxSize={'60'} objectFit={'contain'} />
+      <Heading
+        textAlign={['center', 'left']}
+        maxW={'200px'}
+        size={'sm'}
+        fontFamily={'sans-serif'}
+        noOfLines={3}
+        children={title}
+      />
+      <Text children={description} noOfLines={2} />
+      <HStack>
+        <Text
+          fontWeight={'bold'}
+          textTransform={'uppercase'}
+          children={'Creator'}
+        />
+        <Text
+          fontWeight={'body'}
+          textTransform={'uppercase'}
+          children={creator}
+        />
+      </HStack>
+      <Heading
+        textAlign={'center'}
+        size={'xs'}
+        children={`Lectures - ${lectureCount}`}
+        textTransform={'uppercase'}
+      />
+      <Heading
+        size={'xs'}
+        children={`Views - ${views}`}
+        textTransform={'uppercase'}
+      />
+      <Stack direction={['column', 'row']} alignItems={"center"}>
+        <Link to={`/course/${id}`}>
+          <Button colorScheme='yellow'>Watch Now</Button>
+        </Link>
+        <Button variant={"ghost"} colorScheme='yellow' onClick={() => addToPlaylistHandler(id)}>Add to Playlist</Button>
+      </Stack>
+    </VStack>
+  );
+};
 
 const Courses = () => {
-  return (
-    <div>Courses</div>
-  )
-}
+  const [keyword, setKeyword] = useState('');
+  const [category, setCategory] = useState('');
+  const addToPlaylistHandler = () => {
 
-export default Courses
+  }
+
+  const categories = [
+    'Web Development',
+    'Artificial Intellegence',
+    'Data Structure & Algorithm',
+    'App Development',
+    'Data Science',
+    'Game Development',
+  ];
+  return (
+    <Container minH={'95vh'} maxW={'container.lg'} padding={'8'}>
+      <Heading children="All Courses" m={'8'} />
+
+      <Input
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        placeholder="Search a course"
+        type="text"
+        focusBorderColor="yellow.500"
+        mb={'6'}
+      />
+
+      <HStack
+        overflowX={'auto'}
+        paddingY={'8'}
+        css={{
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        }}
+      >
+        {categories?.map((item, index) => (
+          <Button minW={'60'} onClick={() => setCategory(item)}>
+            <Text children={item} />
+          </Button>
+        ))}
+      </HStack>
+
+      <Stack
+        direction={['column', 'row']}
+        flexWrap={'wrap'}
+        justifyContent={['flex-start', 'space-evenly']}
+        alignItems={['center', 'flex-start']}
+      >
+        <Course
+          title={'Sample1'}
+          description={'Smaple1'}
+          views={23}
+          imageSrc={'sample1'}
+          id={'sample1'}
+          creator={'sample1'}
+          lectureCount={2}
+          addToPlaylistHandler={addToPlaylistHandler}
+        />
+      </Stack>
+    </Container>
+  );
+};
+
+export default Courses;
