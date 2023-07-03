d155fc9713d51c4ebc20beaa90498ed1d0737da4
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-07-03 12:03:28 +0530

    about-page-added@12:03

--- course-builder-app/src/App.js
+++ course-builder-app/src/App.js
@@ -10,6 +10,7 @@ import ForgetPassword from './components/auth/ForgetPassword';
 import ResetPassword from './components/auth/ResetPassword';
 import Contact from './components/contact/Contact'
 import RequestCourse from './components/requestCourse/RequestCourse';
+import About from './components/about/About';
 function App() {
   return (
     <Router>
@@ -20,6 +21,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/forgetpassword' element={<ForgetPassword />} />
         <Route path='/resetpassword/:token' element={<ResetPassword />} />
+        <Route path='/about' element={<About />} />
         <Route path='/courses' element={<Courses />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/request' element={<RequestCourse />} />
