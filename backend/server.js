2ad14fbe6a302d67b0d87b0d766be17c109a2b51
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-07-04 19:00:10 +0530

    database-setUp@19:01

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 import app from './app.js'
+import {connectToMongoDB} from "./config/database.js"
+
+connectToMongoDB()
 
 app.listen(process.env.PORT , () => {
 console.log(`Server is working on port: ${process.env.PORT}`)
