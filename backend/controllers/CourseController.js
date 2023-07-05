f405f42b8ccc848ddae94934ddb12660cdd3a966
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-07-05 22:38:30 +0530

    cloudinary-setUp@22:40

--- backend/controllers/CourseController.js
+++ backend/controllers/CourseController.js
@@ -1,6 +1,9 @@
 import { catchAsyncError } from "../middlewares/CatchAsyncError.js";
 import { Course } from "../models/Course.js";
+import getDataUri from "../utils/DataUri.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
+import cloudinary from 'cloudinary'
+
 export const getAllCourses = catchAsyncError(async (req, res, next) => {
   const courses = await Course.find().select("-lectures");
   res.status(200).json({
@@ -8,12 +11,20 @@ export const getAllCourses = catchAsyncError(async (req, res, next) => {
     courses,
   });
 });
+
 export const createCourse = catchAsyncError(async (req, res, next) => {
   const { title, description, category, createdBy } = req.body;
   if (!title || !description || !category || !createdBy)
     return next(new ErrorHandler("Please add all fields", 400));
 
-//   const file = req.file;
+    const file = req.file;
+    // console.log(file)
+
+    const fileUri = getDataUri(file)
+
+    // console.log(fileUri.content)
+
+    const mycloud = await cloudinary.v2.uploader.upload(fileUri.content)
 
   await Course.create({
     title,
@@ -21,13 +32,58 @@ export const createCourse = catchAsyncError(async (req, res, next) => {
     category,
     createdBy,
     poster: {
-      public_id: "temp",
-      url: "temp",
+      public_id: mycloud.public_id,
+      url: mycloud.secure_url,
     },
   });
 
   res.status(201).json({
     success: true,
     message: "Course Created Successfully. You can add lectures now",
+  });
+});
+
+export const getCourseLecture = catchAsyncError(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) return next(new ErrorHandler("Course not found", 404));
+
+  course.views += 1;
+
+  await course.save();
+
+  res.status(201).json({
+    success: true,
+    lectures: course.lectures,
+  });
+});
+
+export const addLecture = catchAsyncError(async (req, res, next) => {
+
+  const {id} = req.params
+
+  const {title, description} = req.body
+  // const file = req.file
+
+  const course = await Course.findById(id);
+
+  if (!course) return next(new ErrorHandler("Course not found", 404));
+
+  // upload file here
+
+  course.lectures.push({
+    title,description,video:{
+      public_id:"url",
+      url:"url",
+    },
+  })
+
+  course.numOfVideos = course.lectures.length
+
+  await course.save();
+
+  res.status(201).json({
+    success: true,
+    message:"Lecture added in course"
   });
 });
--- backend/routes/CourseRoutes.js
+++ backend/routes/CourseRoutes.js
@@ -1,15 +1,22 @@
-import express from "express"
-import { getAllCourses, createCourse } from "../controllers/CourseController.js";
+import express from "express";
+import {
+  getAllCourses,
+  createCourse,
+  getCourseLecture,
+  addLecture
+} from "../controllers/CourseController.js";
+import singleUpload from "../middlewares/Multer.js";
 
 const router = express.Router();
 
 // Get all courses without lectures
-router.route("/courses").get(getAllCourses)
+router.route("/courses").get(getAllCourses);
 
 // create new course - only admins
-router.route("/createcourse").post(createCourse)
+router.route("/createcourse").post( singleUpload ,createCourse);
 
 // Add lectures, delete course, get course details
+router.route("/course/:id").get(getCourseLecture).post(singleUpload ,addLecture)
 
 // delete lectures
 
