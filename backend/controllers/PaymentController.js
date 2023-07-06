d26ec872de904b81be09553e27f1c871b20a0edf
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-07-06 22:39:52 +0530

    payment-model-paymentverification-api-added@22:40

--- backend/controllers/PaymentController.js
+++ backend/controllers/PaymentController.js
@@ -2,6 +2,8 @@ import { catchAsyncError } from "../middlewares/CatchAsyncError.js";
 import { User } from "../models/User.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
 import { instance } from "../server.js";
+import crypto from "crypto";
+import { Payment } from "../models/Payment.js";
 
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
@@ -26,5 +28,45 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
   res.status(201).json({
     success: true,
     subscriptionId: subscription.id,
+  });
+});
+
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+    req.body;
+  const user = await User.findById(req.user._id);
+
+  const subscription_id = user.subscription.id;
+
+  const generated_signature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
+
+  const isAuthentic = generated_signature === razorpay_signature;
+
+  if (!isAuthentic)
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfailed`);
+
+  // database comes here
+  await Payment.create({
+    razorpay_payment_id,
+    razorpay_order_id,
+    razorpay_signature,
+  });
+
+  user.subscription.status = "active";
+
+  await user.save();
+
+  res.redirect(
+    `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
+  );
+});
+
+export const getRazorpayKey = catchAsyncError(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    key: process.env.RAZORPAY_API_KEY,
   });
 });
--- backend/routes/PaymentRoutes.js
+++ backend/routes/PaymentRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/Auth.js";
-import { buySubscription } from "../controllers/PaymentController.js";
+import { buySubscription, getRazorpayKey, paymentVerification } from "../controllers/PaymentController.js";
 
 
 const router = express.Router();
 
 // Buy Subscription
-
 router.route("/subscribe").get(isAuthenticated, buySubscription)
 
+// verify payment and save reference in database
+router.route("/paymentverification").post(isAuthenticated, paymentVerification)
+
+// get Razorpay key
+router.route("/razorpaykey").get( getRazorpayKey)
 export default router;
