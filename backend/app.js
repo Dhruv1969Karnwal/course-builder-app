bfe3c8c12782f759b1ce3dbf81e27408db573572
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-07-08 09:42:39 +0530

    cors-added@09:45

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 config({
   path: "./config/config.env",
@@ -16,6 +17,12 @@ app.use(
   })
 );
 app.use(cookieParser())
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true,
+  methods:["GET","POST","PUT","DELETE"],
+  
+}))
 
 import course from "./routes/CourseRoutes.js";
 import user from "./routes/UserRoutes.js";
@@ -28,5 +35,7 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
+
+app.get("/", (req,res) => res.send(`<h1> Site is working, click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`))
 
 app.use(ErrorMiddleware);
